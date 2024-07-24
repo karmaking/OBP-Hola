37f1700b537de758bb012409f16b3ea3f95dfe10
karmaking <daniel@danieltippmann.de>
2024-07-24 12:26:07 +0200

    WIP testing

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ WORKDIR /
 COPY target/obp-hola-app-*-SNAPSHOT.jar obp-hola.jar
 COPY application.properties application.properties
 EXPOSE 8087
-ENTRYPOINT ["java","-jar","/obp-hola.jar"]
-#ENTRYPOINT ["/entrypoint.sh"]
+#ENTRYPOINT ["java","-jar","/obp-hola.jar"]
+ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
-echo $KEYSTORE_BASE64 | base64 -d > /etc/ssl/certs/keystore.jks
-echo $TRUSTSTORE_BASE64 | base64 -d > /etc/ssl/certs/truststore.jks
+#echo $KEYSTORE_BASE64 | base64 -d > /etc/ssl/certs/keystore.jks
+#echo $TRUSTSTORE_BASE64 | base64 -d > /etc/ssl/certs/truststore.jks
 exec java -jar /obp-hola.jar
+sleep 300
